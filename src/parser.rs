b65cbd6db6130b6b1d4141bb3a12f43e4a01093f
Billy Levin <billy.levin97@gmail.com>
2022-06-09 21:34:42 +0100

    add some error checking to parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -9,6 +9,8 @@ pub struct Parser<'a> {
 
     current_token: Token,
     peek_token: Token,
+
+    errors: Vec<String>,
 }
 
 impl<'a> Parser<'a> {
@@ -17,6 +19,7 @@ impl<'a> Parser<'a> {
             lexer,
             current_token: Token::Eof,
             peek_token: Token::Eof,
+            errors: Vec::new(),
         };
 
         // read two tokens so that `current_token` and `peek_token` both get set
@@ -24,6 +27,10 @@ impl<'a> Parser<'a> {
         parser.next_token();
 
         parser
+    }
+
+    pub fn errors(self) -> Vec<String> {
+        self.errors
     }
 
     pub fn parse_program(&mut self) -> Program {
@@ -55,7 +62,10 @@ impl<'a> Parser<'a> {
     fn parse_let_statement(&mut self) -> Option<Statement> {
         match self.peek_token {
             Token::Identifier(_) => self.next_token(),
-            _ => return None,
+            _ => {
+                self.no_identifier_error();
+                return None;
+            }
         }
 
         let identifier = match &self.current_token {
@@ -89,19 +99,49 @@ impl<'a> Parser<'a> {
     }
 
     fn expect_peek(&mut self, token: Token) -> bool {
-        if self.peek_token_is(token) {
+        if self.peek_token_is(token.clone()) {
             self.next_token();
             true
         } else {
+            self.peek_error(token);
             false
         }
+    }
+
+    fn peek_error(&mut self, token: Token) {
+        self.errors.push(format!(
+            "expected next token to be {:?}, got {:?} instead",
+            token, self.peek_token
+        ));
+    }
+
+    fn no_identifier_error(&mut self) {
+        self.errors.push(format!(
+            "expected some Identifier token, got {:?} instead",
+            self.peek_token
+        ));
     }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
+
+    fn check_parser_errors(parser: Parser) {
+        let errors = parser.errors();
+
+        if errors.len() == 0 {
+            return;
+        }
+
+        println!("parser has {} errors", errors.len());
+
+        for error in errors {
+            println!("parser error: {}", error);
+        }
+
+        panic!("errors found");
+    }
 
     #[test]
     fn parse_let_statements() {
@@ -115,6 +155,8 @@ mod tests {
         let mut parser = Parser::new(lexer);
 
         let program = parser.parse_program();
+
+        check_parser_errors(parser);
 
         // TODO: make this parse the full statement - not just the identifiers
         assert_eq!(
